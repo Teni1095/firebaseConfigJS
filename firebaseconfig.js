4b8dca2b04d89435da28d859dbdbc8d12ae348ed
Teni Jacob Joseph <tenijacob@gmail.com>
2021-04-18 03:46:18 +0100

    Added more options
    Added options to 
    add team members, 
    send requests to get involved, 
    View count for each topic

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -16,19 +16,21 @@ var auth = firebase.auth();
 
 
 class User{
-  constructor (email, name){
+  constructor (email, name, id){
     this.email = email;
     this.name = name;
+    this.id = id;
   }
 }
 
-async function getCurrentUser(){
+//returns current user
+async function getCurrentUser(){ 
   var user = auth.currentUser;
   if(user!=null){
   var userData;
   var snapshot = await db.collection("users").where("email", "==", user.email).get();
   snapshot.docs.forEach((userVal)=>{
-    userData = new User(userVal.data()["email"], userVal.data()["name"]);
+    userData = new User(userVal.data()["email"], userVal.data()["name"], userVal.id);
   });
   return userData;
   }
@@ -37,13 +39,14 @@ async function getCurrentUser(){
   }
 }
 
-async function getCurrentUserRef(){
+//returns current user id
+async function getCurrentUserId(){
   var user = auth.currentUser;
   if(user!=null){
-  var ref;
+  var ref = null;
   var snapshot = await db.collection("users").where("email", "==", user.email).get();
   snapshot.docs.forEach((userVal)=>{
-    ref = userVal.ref;
+    ref = userVal.ref.id;
   });
   return ref;
   }
@@ -52,8 +55,8 @@ async function getCurrentUserRef(){
   }
 }
 
-async function check_if_user_exists(email) {
 
+async function checkIfUserExists(email) {
   var length = 0;
   await db.collection("users").where("email", "==", email)
   .get()
@@ -70,13 +73,14 @@ async function check_if_user_exists(email) {
   return true;
 }
 
-async function signIn() { //Use this function to sign in via Google
+//Use this function to sign in via Google
+async function signIn() { 
   var provider = new firebase.auth.GoogleAuthProvider();
-	await firebase.auth().signInWithPopup(provider).then(async (result) => {
+	await firebase.auth().signInWithRedirect(provider).then(async (result) => {
     /** @type {firebase.auth.OAuthCredential} */
     
   var user = result.user;
-  var userExists = await check_if_user_exists(user.email);
+  var userExists = await checkIfUserExists(user.email);
 	if(!userExists){
 	var db = firebase.firestore();  
 	db.collection("users").doc().set({
@@ -89,15 +93,140 @@ async function signIn() { //Use this function to sign in via Google
 .catch((error) => {
     console.error("Error writing document: ", error);
 });}
-    // ...
-  });  // The function returns the product of p1 and p2
+  
+  });  
 }
 
-class Title{
-  constructor (title, description){
+class Topic{
+
+  constructor (title, description, owner, id, viewCount = 0){
     this.title = title;
     this.description = description;
+    this.owner = owner;
+    this.id = id;
+    this.viewCount = viewCount;
   }
 }
 
-export  {signIn, getCurrentUser};
+//Can be used to increase view count
+async function increaseViewCount(id){
+  await db.collection("topics").doc(id).update({viewCount:firebase.firestore.FieldValue.increment(1)});
+}
+
+//Create a post topic
+async function createTopic(title, description) { //for creating a topic
+  var owner = await getCurrentUserId();
+  var coll = await db.collection("topics").add({
+    title:title,
+    description:description,
+    owner:owner,
+    viewCount: 0
+  });
+  var topic = new Topic(title, description, owner, coll.id);
+  return topic;
+}
+
+//Get a topic from ID
+async function getTopic(id) { //for creating a topic
+  var coll = await db.collection("topics").doc(id).get();
+  var data = coll.data();
+  var topic = new Topic(data["title"], data["description"], data["owner"], coll.id);
+  return topic;
+}
+
+//Get a topic for people to choose and join
+async function getAllTopics() { 
+  var list = [];
+  var coll = await db.collection("topics").get();
+  coll.docs.forEach((topic)=>{
+    var data = topic.data();
+    var newTopic = new Topic(data["title"], data["description"], data["owner"], data.id);
+    list.push(newTopic);
+  });
+  return list;
+}
+
+//Get a topic where user is owner
+async function getAllOwnerTopics() { 
+  var owner = await getCurrentUserId();
+  var list = [];
+  var coll = await db.collectionGroup("topics").where("owner", "==", owner).get();
+  coll.docs.forEach((topic)=>{
+    var data = topic.data();
+    var newTopic = new Topic(data["title"], data["description"], data["owner"], topic.id);
+    list.push(newTopic);
+  });
+  console.log(list);
+  return list;
+}
+
+//Get a topic where user is a member
+async function getAllMemberTopics() { 
+  var userId = await getCurrentUserId();
+  var list = [];
+  var topicMembers = await db.collectionGroup("members").where("memberId", "==", userId).get();
+  topicMembers.docs.forEach(async (members)=>{
+    var topic = await getTopic(members.data()["topicId"]);
+    list.push(topic);
+  });
+  return list;
+}
+
+class TopicMember {
+  constructor (topicId, memberId, id){
+    this.topicId = topicId;
+    this.memberId = memberId;
+    this.id = id;
+  }
+}
+
+//Add user to be a member (or to get involved)
+async function addMember (topicId, memberId) {
+  var doc = await db.collection("topics").doc(topicId).collection("members").add({
+    topicId : topicId,
+    memberId : memberId,
+  });
+  var member = new TopicMember(topicId, memberId, doc.id);
+  return member;
+}
+
+class GetInvolved {
+  constructor(fromUser, toTopic, id){
+    this.fromUser = fromUser;
+    this.toTopic = toTopic;
+    this.id = id;
+  }
+}
+
+//Sends a request to get involved to the owner of the topic
+async function getInvolved(toTopicId){
+  var userId = await getCurrentUserId();
+  var owner = (await getTopic(toTopicId)).owner;
+  var doc = await db.collection("users").doc(owner).collection("getInvolvedRequests").add({
+    fromUser : userId,
+    toTopic : toTopicId    
+  });
+  var request = new GetInvolved(userId, toTopicId, doc.id);
+  return request;
+}
+
+//Deletes the request
+async function deleteGetInvolvedRequest(requestId){
+  var userId = await getCurrentUserId();
+  await db.collection("users").doc(userId).collection("getInvolvedRequests").doc(requestId).delete();
+}
+
+//Gets all requests for the user
+async function getAllRequests() { 
+  var list = [];
+  var userId = await getCurrentUserId();
+  var coll = await db.collection("users").doc(userId).collection("getInvolvedRequests").get()
+  coll.docs.forEach((request)=>{
+    var data = request.data();
+    var newRequest = new GetInvolved(data["fromUser"], data["toTopic"], data.id);
+    list.push(newRequest);
+  });
+  return list;
+}
+
+export  {signIn, getCurrentUser,createTopic, getTopic, getAllTopics,increaseViewCount, getInvolved, addMember, getAllRequests, deleteGetInvolvedRequest, getAllMemberTopics, getAllOwnerTopics};
